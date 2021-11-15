162441ab08348a47522e8ad111bacb164cb43eac
AkankGit123 <akankshacsengg@gmail.com>
2021-11-15 15:50:24 +0530

    Stream

---
+++ Streamst.java
@@ -0,0 +1,12 @@
+package com.rays.Stream;
+
+import java.util.ArrayList;
+
+public class Streamst {
+	public static void main(String[] args) {
+		
+
+ArrayList<Person> list = new ArrayList<E>()
+	}
+	
+}
